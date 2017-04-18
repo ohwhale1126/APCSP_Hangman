0f30946a9538a3c6126ccee1d087c95fdba34995
Gemma Longinotti <Gem@Gemmas-MacBook-Pro.local>
2017-04-18 08:53:50 -0700

    fleshing out skeleton

--- Hangman.js
+++ Hangman.js
@@ -8,13 +8,14 @@ window.onload = function () {
   var word ;              // Selected word
   var hiddenWord;         //selected word with letters replaced by "-"
   var guess ;             // Geuss
-  var guesses = [ ];      // Stored guesses: array
+  var wrongLetter = [];   //incorrect guess
+  var correctLetter = []; //correct guess
   var lives ;             // Lives
-  var counter ;           // Count correct guesses
-  var space;              // Number of spaces in word '-'
 
 
-    //1. get word 2. hide word 3. user guesses. 4
+    //1. get word 2. hide word 3.the user guesses incorrect 3.1. subtract a life 3.2.add to incorrect array 3.3. add to canvas ||| 4. user guesses correct 4.1 change correct dash to letter 4.2 add to correct letter array
 
 function getWord(){}
 function hideWord(){}
+function showLives(){}
+
