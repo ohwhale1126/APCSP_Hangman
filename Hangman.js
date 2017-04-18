7def99ab905bdb8f552bc881d867371fb18591e8
Gemma Longinotti <Gem@Gemmas-MacBook-Pro.local>
2017-04-18 09:08:25 -0700

    getword function added

--- Hangman.js
+++ Hangman.js
@@ -15,7 +15,10 @@ window.onload = function () {
 
     //1. get word 2. hide word 3.the user guesses incorrect 3.1. subtract a life 3.2.add to incorrect array 3.3. add to canvas ||| 4. user guesses correct 4.1 change correct dash to letter 4.2 add to correct letter array
 
-function getWord(){}
+function getWord(){
+          var terms = ["function", "variable", "string", "number", "boolean", "Qunit", "Math", "camelcase", "cascading style sheets", "binary", "hexadecimal", "bootstrap", "github", "modulus", "html", "javascript"];
+          var word = terms[Math.floor(Math.random() * terms.length)];
+}
 function hideWord(){}
 function showLives(){}
 
