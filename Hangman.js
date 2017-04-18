30224d7b199192abea414ef91b3c0c7e1c75fa5b
Gemma Longinotti <Gem@Gemmas-MacBook-Pro.local>
2017-04-18 08:47:50 -0700

    beginning skeleton

--- Hangman.js
+++ Hangman.js
@@ -6,119 +6,15 @@ window.onload = function () {
 
   var terms = [ ];
   var word ;              // Selected word
+  var hiddenWord;         //selected word with letters replaced by "-"
   var guess ;             // Geuss
-  var guesses = [ ];      // Stored guesses
+  var guesses = [ ];      // Stored guesses: array
   var lives ;             // Lives
   var counter ;           // Count correct guesses
   var space;              // Number of spaces in word '-'
 
-  // Get elements
-  var showLives = document.getElementById("mylives");
 
-  // create alphabet ul
-  var buttons = function () {
-    myButtons = document.getElementById('buttons');
-    letters = document.createElement('ul');
+    //1. get word 2. hide word 3. user guesses. 4
 
-    for (var i = 0; i < alphabet.length; i++) {
-      letters.id = 'alphabet';
-      list = document.createElement('li');
-      list.id = 'letter';
-      list.innerHTML = alphabet[i];
-      check();
-      myButtons.appendChild(letters);
-      letters.appendChild(list);
-    }
-  }
-
-
-  // Create guesses ul
-   result = function () {
-    wordHolder = document.getElementById('hold');
-    correct = document.createElement('ul');
-
-    for (var i = 0; i < word.length; i++) {
-      correct.setAttribute('id', 'my-word');
-      guess = document.createElement('li');
-      guess.setAttribute('class', 'guess');
-      if (word[i] === "-") {
-        guess.innerHTML = "-";
-        space = 1;
-      } else {
-        guess.innerHTML = "_";
-      }
-
-      guesses.push(guess);
-      wordHolder.appendChild(correct);
-      correct.appendChild(guess);
-    }
-  }
-
-  // Show lives
-   comments = function () {
-    showLives.innerHTML = "You have " + lives + " lives";
-    if (lives < 1) {
-      showLives.innerHTML = "Game Over";
-    }
-    for (var i = 0; i < geusses.length; i++) {
-      if (counter + space === guesses.length) {
-        showLives.innerHTML = "You Win!";
-      }
-    }
-  }
-      // Hangman
-
-  // OnClick Function
-   check = function () {
-    list.onclick = function () {
-      var guess = (this.innerHTML);
-      this.setAttribute("class", "active");
-      this.onclick = null;
-      for (var i = 0; i < word.length; i++) {
-        if (word[i] === guess) {
-          guesses[i].innerHTML = guess;
-          counter += 1;
-        }
-      }
-      var j = (word.indexOf(guess));
-      if (j === -1) {
-        lives -= 1;
-        comments();
-          } else {
-        comments();
-      }
-    }
-  }
-
-
-  // Play
-  play = function () {
-   terms = ["function","variable","string","number","boolean","Qunit","Math","camelcase","cascading style sheets","binary","hexadecimal","bootstrap","github","modulus","html","javascript"]
-word = terms[Math.floor(Math.random() * terms.length)];
-    word = word.replace(/\s/g, "-");
-    console.log(word);
-    buttons();
-
-    guesses = [ ];
-    lives = 10;
-    counter = 0;
-    space = 0;
-    result();
-    comments();
-
-  };
-
-  play();
-
-
-   // Reset
-
-  document.getElementById('reset').onclick = function() {
-    correct.parentNode.removeChild(correct);
-    letters.parentNode.removeChild(letters);
-    play();
-  }
-}
-
-
-
+function getWord(){}
+function hideWord(){}
